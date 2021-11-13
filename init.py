c13eefb07c3287786cd2b772ffd85b31fb053fba
Messaouali Abdeslem <abdeslem1803@gmail.com>
2021-11-13 23:41:14 +0100

    Initial commit

---
+++ init.py
@@ -0,0 +1,11 @@
+import eel
+
+# Set web files folder and optionally specify which file types to check for eel.expose()
+#   *Default allowed_extensions are: ['.js', '.html', '.txt', '.htm', '.xhtml']
+eel.init('web', allowed_extensions=['.js', '.html'])
+
+@eel.expose                         # Expose this function to Javascript
+def say_hello_py(x):
+    print('Hello from %s' % x)
+
+eel.start('hello.html')
---
+++ web/app.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import { Button } from '@material-ui/core';
+
+function App() {
+    return (
+      <div >
+        <header >
+          <Button>Hello world</Button>
+          <h1>Hello World</h1>
+        </header>
+      </div>
+    );
+  }
+  
+  export default App;
+  
